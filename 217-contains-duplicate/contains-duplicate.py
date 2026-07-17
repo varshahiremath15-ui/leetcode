4a72e92b14113a31fb069a1c614ebdf0a64436dd
varshahiremath15-ui <varshahiremath15@gmail.com>
2026-07-17 15:00:50 +0530

    Time: 16 ms (76.74%) | Memory: 25.9 MB (61.49%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        if(len(set(nums))==len(nums)):
+            return False
+        else:
+            return True
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
