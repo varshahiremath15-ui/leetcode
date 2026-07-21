275d84413a4929d016d555e4d17b3b66d32c6092
varshahiremath15-ui <varshahiremath15@gmail.com>
2026-07-21 18:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (7.26%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char>st;
+        for(char ch:sentence)
+        {
+            st.insert(ch);
+        }
+        return st.size()==26;
+    }
+};
