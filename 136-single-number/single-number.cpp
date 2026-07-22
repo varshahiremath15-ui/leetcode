ae948467116ee7a7100b1f0a550e3713ae515315
varshahiremath15-ui <varshahiremath15@gmail.com>
2026-07-22 11:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (59.13%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
-        int n=nums.size();
-        for(int i:nums)
+        for (int i:nums)
         {
             ans^=i;
         }
         return ans;
-
         
     }
 };
