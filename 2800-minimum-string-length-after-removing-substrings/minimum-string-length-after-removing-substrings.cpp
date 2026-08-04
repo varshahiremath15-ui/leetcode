9508771d6c3b4e29f5c317e7675aaee021bd2fc8
varshahiremath15-ui <varshahiremath15@gmail.com>
2026-08-04 19:09:12 +0530

    Time: 7 ms (13.54%) | Memory: 11.8 MB (25.43%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(auto ch:s)
+        { if(!st.empty()&&
+        ((st.top()=='A' && ch=='B')||
+        (st.top()=='C'&& ch=='D')))
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(ch);
+        }
+        }
+        return st.size();
+    }
+};
