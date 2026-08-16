d079e7c9faef58dad8547459d89e4a5993f9cfe3
varshahiremath15-ui <varshahiremath15@gmail.com>
2026-08-16 20:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (57.86%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        for(int i=0;i<strs[0].length();i++)
+        {
+            char ch=strs[0][i];
+            for(int j=1;j<strs.size();j++)
+            {
+                if(i>=strs[j].length()|| strs[j][i]!=ch)
+                {
+                    return ans;
+                }
+            }
+            ans+=ch;
+        }
+        return ans;
+        
+    }
+};
